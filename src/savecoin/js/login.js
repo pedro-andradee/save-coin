9b8c7258de59a28db737e2f48500a9c7e8a0b091
Giovanny Brito <giovannyprofissional@gmail.com>
2022-11-01 16:55:57 -0300

    adjusting login 01/11/2022

--- src/savecoin/js/login.js
+++ src/savecoin/js/login.js
@@ -1,28 +1,3 @@
-
-// variavel chamando o icone de olho <pela classe> para mostrar a senha. 
-let eye= document.querySelector('.bi-eye-fill')
-
-
-//add um evento quando clicar. (arrow function = "faça o que eu colocar nas chaves").
-
-.addEventListener('click', ()=>{
-    // pegar o input de senha e colocar dentro da variavel em questão.
-    let inputSenha = document.querySelector('#campo-senha')
-
-    //se o atributo:type do 'inputSenha' for password "faça o que eu colocar nas chaves".
-    if (inputSenha.getAttribute('type') == 'password'){
-
-        //insira no atributo type o tipo text.
-        inputSenha.setAttribute('type', 'text')
-    }
-
-    else{
-        input.setAttribute('type', 'password')
-    }
-})
-
-//parar aqui
-
 //SCRIPT (DO HTML) LOGIN ROMMEL >> INICIO
  
         // Declara uma função para processar o formulário de login
