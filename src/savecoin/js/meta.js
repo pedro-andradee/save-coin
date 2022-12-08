7141206b75fd225635b1085cb2c0c68d063b2bc1
Pedro Andrade <andradephc11@gmail.com>
2022-12-08 11:03:53 -0300

    Atualizacao add meta

--- src/savecoin/js/meta.js
+++ src/savecoin/js/meta.js
@@ -12,6 +12,23 @@ function initDadosMeta() {
     localStorage.setItem('BancoUsuarios', JSON.stringify(BancoUsuarios));
 }
 
+// Fonte: https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
+function generateUUID() { // Public Domain/MIT
+    const d = new Date().getTime();//Timestamp
+    const d2 = (performance && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if (d > 0) {//Use timestamp until depleted
+            r = (d + r) % 16 | 0;
+            d = Math.floor(d / 16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r) % 16 | 0;
+            d2 = Math.floor(d2 / 16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
 function carregarRegistrosMetaUsuarioCorrente() {
     var usuarioDoBanco = BancoUsuarios.usuarios;
     var qtdeUsuarios = usuarioDoBanco.length;
@@ -38,29 +55,30 @@ function carregarMetas() {
         if (getMetasUsuario.metas[i].id !== null) {
             divBlocoMeta.innerHTML = `
                 <div class="conteudoMetas">
-                <img
-                    class="cofrePorco"
-                    src="../img/piggy-bank.svg"
-                    alt="cofrinho porco"
-                />
-                <div class="containerMeta">
-                    <span class="descricaoMeta">${getMetasUsuario.metas[i].descricaoMeta}</span>
-                    <div class="barraProgresso">
-                        <div class="barraVerde preenchimentoBarra${i.toString()}"></div>
+                    <img
+                        class="cofrePorco"
+                        src="../img/piggy-bank.svg"
+                        alt="cofrinho porco"
+                    />
+                    <div class="containerMeta">
+                        <span class="descricaoMeta">${getMetasUsuario.metas[i].descricaoMeta}</span>
+                        <div class="barraProgresso">
+                            <div class="barraVerde preenchimentoBarra${i.toString()}"></div>
+                        </div>
+                        <div class="containerDataProgresso">
+                            <span class="dataMeta">Data final: ${getMetasUsuario.metas[i].dataMeta}</span>
+                            <span class ="valorMeta">Meta: R$ ${getMetasUsuario.metas[i].valorMeta.toFixed(2)}&nbsp;|&nbsp;Faltam: R$ ${(getMetasUsuario.metas[i].valorMeta - getMetasUsuario.metas[i].valorDepositado).toFixed(2)}</span>
+                        </div>
                     </div>
-                    <div class="containerDataProgresso">
-                        <span class="dataMeta">Data final: ${getMetasUsuario.metas[i].dataMeta}</span>
-                        <span class ="valorMeta">Meta: R$ ${getMetasUsuario.metas[i].valorMeta.toFixed(2)}&nbsp;|&nbsp;Faltam: R$ ${(getMetasUsuario.metas[i].valorMeta - getMetasUsuario.metas[i].valorDepositado).toFixed(2)}</span>
+                    <div class="containerDelete" id="${getMetasUsuario.metas[i].id}">
+                        <button
+                        type="button"
+                        data-toggle="modal"
+                        data-target="#modal-delete-meta"
+                        class="bi bi-x-circle botaoDelete"
+                        onclick="getIdMeta(event)"
+                        ></button>
                     </div>
-                </div>
-                <div class="containerDelete">
-                    <button
-                    type="button"
-                    data-toggle="modal"
-                    data-target="#modal-delete-meta"
-                    class="bi bi-x-circle botaoDelete"
-                    ></button>
-                </div>
                 </div>
                 <button
                 type="button"
@@ -95,19 +113,39 @@ function carregarBarraProgresso() {
 }
 
 function salvarMeta() {
-   let descricaoMeta = document.querySelector("#descricaoMeta").value;
+    let descricaoMeta = document.querySelector("#descricaoMeta").value;
     let valorMeta = Number.parseFloat(document.querySelector("#valorMeta").value);
     let dataMeta = document.querySelector("#dataMeta").value;
     var dataFormatada = dataMeta.split('-').reverse().join('/');
     let registrosMetaUsuario = carregarRegistrosMetaUsuarioCorrente();
-    let meta = {"id": registrosMetaUsuario.metas.length + 1, "descricaoMeta": descricaoMeta, "valorMeta": valorMeta,
+    let metaUUID = generateUUID();
+    let meta = {"id": metaUUID, "descricaoMeta": descricaoMeta, "valorMeta": valorMeta,
     "valorDepositado": 0, "dataMeta": dataFormatada}
     registrosMetaUsuario.metas.push(meta)
 
     localStorage.setItem('BancoUsuarios', JSON.stringify(BancoUsuarios));
+    window.location.href = 'verApagarMeta.html'
 }
 
 
 initDadosMeta();
 carregarMetas();
 carregarBarraProgresso();
+
+function getIdMeta(event) {
+    let containerDelete = event.target.parentNode.id;
+    return containerDelete;
+}
+
+function deletarMeta(event) {
+    let containerDelete = getIdMeta(event);
+    let getMetasUsuarioCorrente = carregarRegistrosMetaUsuarioCorrente();
+    for(let i = 0; i < getMetasUsuarioCorrente.metas.length;i++) {
+        if(getMetasUsuarioCorrente.metas[i].id == containerDelete) {
+            getMetasUsuarioCorrente.metas.splice(i, 1);
+            break;
+        }
+    }
+    localStorage.setItem('BancoUsuarios', JSON.stringify(BancoUsuarios));
+    window.location.href = 'verApagarMeta.html'
+}
