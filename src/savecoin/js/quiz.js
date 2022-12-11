84b06b4b04ea86401e8d2af07df953661d99e627
Roma Medeiros <romario.medeiros@engenharia.ufjf.br>
2022-12-11 16:25:30 -0300

    Etapa 04

--- src/savecoin/js/quiz.js
+++ src/savecoin/js/quiz.js
@@ -1,22 +1,22 @@
 let titulo = document.querySelector('h1')
 let instrucoes = document.querySelector('#instrucoes')
 let aviso = document.querySelector('#aviso')
+let progresso = document.querySelector('#progresso')
+let pontos = 0
+let placar = 0
 
-let pontos = 0 
-let placar = 0 
-
+let somAcerto = document.querySelector('#somAcerto')
+let somErro = document.querySelector('#somErro')
+let somAplausos = document.querySelector('#somAplausos')
 
 let numQuestao = document.querySelector('#numQuestao')
 let pergunta = document.querySelector('#pergunta')
-
 
 let a = document.querySelector('#a')
 let b = document.querySelector('#b')
 let c = document.querySelector('#c')
 
-
 let articleQuestoes = document.querySelector('.questoes')
-
 let alternativas = document.querySelector('#alternativas')
 
 const q0 = {
@@ -30,7 +30,7 @@ const q0 = {
 
 const q1 = {
     numQuestao: 1,
-    pergunta: "Segundo o Método 50/30/20:",
+    pergunta: "Ao aplicar o Método 50/30/20:",
     alternativaA: "50% da renda mensal é destinada às despesas variáveis.",
     alternativaB: "30% da renda mensal é destinada às despesas fixas e essenciais.",
     alternativaC: "20% da renda mensal é destinada às prioridades financeiras.",
@@ -41,25 +41,25 @@ const q2 = {
     numQuestao: 2,
     pergunta: "Qual destas é uma boa prática financeira:",
     alternativaA: "Trabalhar com métodos.",
-    alternativaB: "Consumo por impulso.",
+    alternativaB: "Consumir por impulso.",
     alternativaC: "Gastar mais do que ganha.",
-    correta: "Consumo por impulso.",
+    correta: "Trabalhar com métodos.",
 }
 
 const q3 = {
     numQuestao: 3,
     pergunta: "O que é considerado no consumo consciente?",
-    alternativaA: "O valor financeiro do bem.",
-    alternativaB: "O impacto do bem no meio ambiente, na sociedade e financeiro.",
-    alternativaC: "O valor de revenda do bem.",
-    correta: "O impacto do bem no meio ambiente, na sociedade e financeiro.",
+    alternativaA: "O valor financeiro do bem a ser adquirido",
+    alternativaB: "O impacto do bem no meio ambiente e na sociedade.",
+    alternativaC: "O valor de revenda do bem após o breve tempo de uso",
+    correta: "O impacto do bem no meio ambiente e na sociedade.",
 }
 
 const q4 = {
     numQuestao: 4,
     pergunta: "O que é uma reserva de emergência?",
-    alternativaA: "Um valor guardado que você economiza em tempos difíceis.",
-    alternativaB: "Um valor guardado destinado a aproveitar boas opotunidades de negócios.",
+    alternativaA: "Um valor guardado que você economizou em tempos difíceis.",
+    alternativaB: "Um valor guardado que será destinado a aproveitar boas opotunidades de negócios.",
     alternativaC: "Um valor guardado que está disponível para custear gastos necessários que estavam fora do orçamento.",
     correta: "Um valor guardado que está disponível para custear gastos necessários que estavam fora do orçamento.",
 }
@@ -68,18 +68,18 @@ const q5 = {
     numQuestao: 5,
     pergunta: "O cartão de crédito é:",
     alternativaA: "Uma forma de pagamento onde o valor da compra é descontado imediatamente na conta bancária do usuário após a compra.",
-    alternativaB: "Uma forma de pagamento onde o valor da compra é cobrado ao usuário em uma fatura com data posterior à compra.",
-    alternativaC: "Uma forma de pagamento em que você deve recarregar o cartão com dinheiro antes de realizar a compra.",
+    alternativaB: "Uma forma de pagamento em que você deve recarregar o cartão com dinheiro antes de realizar a compra.",
+    alternativaC: "Uma forma de pagamento onde o valor da compra é cobrado ao usuário em uma fatura com data posterior à compra.",
     correta: "Uma forma de pagamento onde o valor da compra é cobrado ao usuário em uma fatura com data posterior à compra.",
 }
 
 const q6 = {
     numQuestao: 6,
-    pergunta: "É um objetivo médio prazo:",
+    pergunta: "É considerado um objetivo médio prazo:",
     alternativaA: "Fazer uma viagem nas férias.",
-    alternativaB: "Fazer uma reforma na casa.",
-    alternativaC: "Comprar um ou vários imóveis.",
-    correta: "Fazer uma reforma na casa.",
+    alternativaB: "Comprar um ou vários imóveis.",
+    alternativaC: "Realizar uma reforma na casa.",
+    correta: "Realizar uma reforma na casa.",
 }
 
 const questoes = [q0, q1, q2, q3, q4, q5, q6]
@@ -93,18 +93,15 @@ let totalDeQuestoes = (questoes.length) - 1
 console.log("Total de questões " + totalDeQuestoes)
 total.textContent = totalDeQuestoes
 
-
 numQuestao.textContent = q1.numQuestao
 pergunta.textContent = q1.pergunta
 a.textContent = q1.alternativaA
 b.textContent = q1.alternativaB
 c.textContent = q1.alternativaC
 
-
 a.setAttribute('value', '1A')
 b.setAttribute('value', '1B')
 c.setAttribute('value', '1C')
-
 
 function proximaQuestao(nQuestao) {
     numero.textContent = nQuestao
@@ -116,6 +113,7 @@ function proximaQuestao(nQuestao) {
     a.setAttribute('value', nQuestao + 'A')
     b.setAttribute('value', nQuestao + 'B')
     c.setAttribute('value', nQuestao + 'C')
+    progresso.value = parseInt(progresso.value) + 1
 }
 
 function bloquearAlternativas() {
@@ -130,46 +128,59 @@ function desbloquearAlternativas() {
     c.classList.remove('bloqueado')
 }
 
-function verificarSeAcertou(nQuestao, resposta) {
+function piscarNoAcerto() {
+    articleQuestoes.classList.remove('errou')
+    articleQuestoes.classList.add('acertou')
+}
 
+function piscarNoErro() {
+    articleQuestoes.classList.remove('acertou')
+    articleQuestoes.classList.add('errou')
+}
+
+function tirarPiscar() {
+    articleQuestoes.classList.remove('acertou')
+    articleQuestoes.classList.remove('errou')
+
+}
+
+function verificarSeAcertou(nQuestao, resposta) {
     let numeroDaQuestao = nQuestao.value
     console.log("Questão " + numeroDaQuestao)
-
     let respostaEscolhida = resposta.textContent
-    
-
     let certa = questoes[numeroDaQuestao].correta
-    
-
     if (respostaEscolhida == certa) {
-        
-        pontos += 10 
+        piscarNoAcerto()
+        somAcerto.play()
+        pontos += 10
     } else {
-       
+        piscarNoErro()
+        somErro.play()
     }
+    setTimeout(() => {
+        tirarPiscar()
+    }, 600);
 
-    
     placar = pontos
     instrucoes.textContent = "Pontos " + placar
 
-  
     bloquearAlternativas()
 
     setTimeout(function () {
-        
         proxima = numeroDaQuestao + 1
-
         if (proxima > totalDeQuestoes) {
             console.log('Fim do Jogo!')
             fimDoJogo()
         } else {
             proximaQuestao(proxima)
         }
-    }, 350)
+    }, 600)
     desbloquearAlternativas()
 }
 
 function fimDoJogo() {
+
+    if (pontos > 40) {somAplausos.play()}
     instrucoes.textContent = "Fim de Jogo!"
     numQuestao.textContent = ""
 
@@ -188,10 +199,10 @@ function fimDoJogo() {
     b.setAttribute('value', '0')
     c.setAttribute('value', '0')
 
-        articleQuestoes.style.display = 'none'
+    articleQuestoes.style.display = 'none'
 
     setTimeout(function () {
         pontos = 0
         location.reload();
-    }, 2000)
+    }, 3000)
 }
